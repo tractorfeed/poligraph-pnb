5d1adc3c019419182f47e26adaa161befa2fe11f
natebenes <natebenes@gmail.com>
2012-04-15 00:20:52 -0500

    fix people finder

---
+++ db.rb
@@ -0,0 +1,5 @@
+dbhost = "edison.stdio.me"
+dbport = 5432
+dbname = "hswapi"
+dbuser = "hswapi"
+dbpass = "h4ck0m4h4;"
--- find_people.rb
+++ find_people.rb
@@ -1,22 +1,25 @@
+require 'yaml'
 require 'pg'
 require './db'
 
-connection = PG::Connection.new(:host => @dbhost, :port => @dbport, :user => @dbuser, :password => @dbpass, :dbname => @dbname)
+connection = false
 fml_names = Array.new
 single_field_names = Array.new
 
 def conn
-  @connection = @connection || PG::Connection.new(:host => @dbhost, :port => @dbport, :user => @dbuser, :password => @dbpass, :dbname => @dbname)
-  return connection
+  @connection = @connection || PG::Connection.new(YAML.load_file('./db.yml'))
+  return @connection
 end
 
 def a1cand
-  res = @connection.exec('SELECT "Candidate Last Name", "Candidate First Name", "Candidate Middle Initial" FROM FORMA1CAND')
-  res.each do |row|
-    if(res['Candidate First Name'] && res['Candidate Last Name'])
-      names.push ({:fname => res['Candidate First Name'].strip, :lname => res['Candidate Last Name'].strip, :mname => res["Candidate Middle Initial"].strip})
+  names = []
+  result = conn.exec('SELECT "Candidate Last Name", "Candidate First Name", "Candidate Middle Initial" FROM FORMA1CAND')
+  result.each do |res|
+    unless(res['Candidate First Name'].nil? || res['Candidate Last Name'].nil?)
+      names.push ({:fname => res['Candidate First Name'].strip, :lname => res['Candidate Last Name'].strip, :mname => (res["Candidate Middle Initial"].nil? ? "" : res["Candidate Middle Initial"].strip)})
     end
   end
+  return names
 end
 
 
