1660b288866f14a2fb159a2dfa16c84917c7a4c0
Dave Loyall <dave@the-good-guys.net>
2012-04-15 15:11:08 -0500

    pulled lib, pri, and Contact from lob form b

---
+++ table_lformb_contact.rb
@@ -0,0 +1,79 @@
+require './polimapper'
+require 'json'
+JOBS = [
+  {
+    :table => 'lformb',
+    :fields => [
+      # {:name => "Document ID", :required=> false},
+      # {:name => "Date Received", :required=> false},
+      # {:name => "Postmark Date", :required=> false},
+      # {:name => "Year Covered", :required=> false},
+      # {:name => "Nature of Filing", :required=> false},
+      # {:name => "Amended Report", :required=> false},
+      # {:name => "Receipts And Expenditures", :required=> false},
+      # {:name => "Summary Line 1", :required=> false},
+      # {:name => "Summary Line 2", :required=> false},
+      # {:name => "Summary Line 3", :required=> false},
+      # {:name => "Summary Line 4", :required=> false},
+      # {:name => "Summary Line 5A", :required=> false},
+      # {:name => "Summary Line 5B", :required=> false},
+      # {:name => "Summary Line 5C", :required=> false},
+      # {:name => "Summary Line 6", :required=> false},
+      # {:name => "Summary Line 7", :required=> false},
+      # {:name => "Summary Line 8", :required=> false},
+      # {:name => "Summary Line 9", :required=> false},
+      # {:name => "Summary Line 10", :required=> false},
+      # {:name => "Summary Line 11A", :required=> false},
+      # {:name => "Summary Line 11B", :required=> false},
+      # {:name => "Summary Line 11C", :required=> false},
+      # {:name => "Summary Line 11D", :required=> false},
+      # {:name => "Summary Line 12A", :required=> false},
+      # {:name => "Summary Line 12B", :required=> false},
+      # {:name => "Summary Line 12C", :required=> false},
+      # {:name => "Summary Line 12D", :required=> false},
+      # {:name => "Summary Line 13", :required=> false},
+      # {:name => "Other Info", :required=> false},
+      # {:name => "Lobbyist ID", :required=> false},
+      # {:name => "Lobbyist Name", :required=> false},
+      # {:name => "Lobbyist Office Address", :required=> false},
+      # {:name => "Lobbyist Office City", :required=> false},
+      # {:name => "Lobbyist Office State", :required=> false},
+      # {:name => "Lobbyist Office Zip", :required=> false},
+      # {:name => "Lobbyist Office Phone", :required=> false},
+      # {:name => "Lobbyist Residence Address", :required=> false},
+      # {:name => "Lobbyist Residence City", :required=> false},
+      # {:name => "Lobbyist Residence State", :required=> false},
+      # {:name => "Lobbyist Residence Zip", :required=> false},
+      # {:name => "Lobbyist Residence Phone", :required=> false},
+      # {:name => "Principal ID", :required=> false},
+      # {:name => "Principal Name", :required=> false},
+      # {:name => "Principal Office Address", :required=> false},
+      # {:name => "Principal Office City", :required=> false},
+      # {:name => "Principal Office State", :required=> false},
+      # {:name => "Principal Office Zip", :required=> false},
+      # {:name => "Principal Office Phone", :required=> false},
+      {:name => "Principal Official Contact", :required=> false}
+      # {:name => "Principal Nature of Business", :required=> false}
+    ],
+    :model => { # polimapper only likes string literals
+
+
+            "name" => {
+              "full" => "Principal Official Contact"
+            },
+            "address" => [],
+            "phone" => [],
+
+
+
+      "source" => {
+        "doc" => :lformb,
+        "id" => "hswid"
+      }
+    }
+  }
+]
+
+JOBS.each do |job|
+  puts PoliMapper.fill_model(job[:table], job[:model], job[:fields]).to_json
+end
